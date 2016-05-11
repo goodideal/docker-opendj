813410f6b41c41f4778577bd60080e79ff9c9890
Dawid Malinowski <dawidmalina@gmail.com>
2016-05-11 17:54:34 +0200

    Small refactor

--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 Basic Docker image to run Opendj-Server
 
 You need edit (add) this env:
-- **LDAPORT**: ldap port default:389
-- **LDAPSPORT**: ldaps port default:636
-- **ADMINPORT**: admin default:4444
-- **BASEDN**: basedn
-- **HOST**: hostname
-- **PASSWORD**: password for master user
-- **DUMPLDAP**: if you want import ldif file, you need write the name of the file and mount directory to tmp with R-W option
+- **LDAPORT**: ldap port (default 389)
+- **LDAPSPORT**: ldaps port (default 636)
+- **ADMINPORT**: admin port (default 4444)
+- **BASEDN**: basedn (default dc=example,dc=com)
+- **HOST**: hostname (default localhost)
+- **PASSWORD**: password for master user (default admin123)
 
+
+- **INIT_LDAP**: this should be set only once to configure ldap instance
+- **LOAD_DUMP**: provide full path to dump file that should be imported
 
 Usage:
 ```
@@ -17,6 +19,6 @@ docker run -d -e BASEDN="dc=test -e HOST=localhost -e PASSWORD=password oberthur
 ```
 or
 ```
-docker run -d -p 389:389 -p 636:636 -e LDAPORT=389 -e LDAPSPORT=636 -e BASEDN="dc=test" -e HOST=localhost -e PASSWORD=password -e DUMPLDAP=test.ldif -v PATHTOLDIFF:/tmp/ oberthur/docker-opendj
+docker run -d -p 389:389 -p 636:636 -e LDAPORT=389 -e LDAPSPORT=636 -e BASEDN="dc=test" -e HOST=localhost -e PASSWORD=password -e LOAD_DUMP=/tmp/test.ldif oberthur/docker-opendj
 
 ```
--- start-opendj.sh
+++ start-opendj.sh
@@ -1,43 +1,35 @@
 #!/bin/bash
 set -e
 
-##ENV
-echo ${CLUSTER}
-echo ${LDAPORT}
-echo ${LDAPSPORT}
-echo ${ADMINPORT}
-echo ${BASEDN}
-echo ${HOST}
-echo ${CLUSTER_NODE}
-echo ${PASSWORD}
-echo ${DUMPLDAP}
+# ENV
+echo ${LDAPORT:-389}
+echo ${LDAPSPORT:-636}
+echo ${ADMINPORT:-4444}
+echo ${BASEDN:-dc=example,dc=com}
+echo ${HOST:-localhost}
+echo "${PASSWORD:-admin123}"
 
-install_LDAP(){
-  /opt/opendj/setup --cli --ldapPort ${LDAPORT:-389} --ldapsPort ${LDAPSPORT:-636} --adminConnectorPort ${ADMINPORT:-4444} --enableStartTLS --generateSelfSignedCertificate --baseDN ${BASEDN} -h ${HOST:-localhost} --rootUserPassword "${PASSWORD:-admin123}" --acceptLicense --no-prompt
+initLdap(){
+  /opt/opendj/setup --cli --ldapPort ${LDAPORT:-389} --ldapsPort ${LDAPSPORT:-636} --adminConnectorPort ${ADMINPORT:-4444} --enableStartTLS --generateSelfSignedCertificate --baseDN ${BASEDN:-dc=example,dc=com} -h ${HOST:-localhost} --rootUserPassword "${PASSWORD:-admin123}" --acceptLicense --no-prompt
 }
 
 stopServer(){
   /opt/opendj/bin/stop-ds
 }
 
-loadFile(){
-  /opt/opendj/bin/import-ldif --includeBranch "${BASEDN}" --backendID userRoot --ldifFile /tmp/"${DUMPLDAP}"
+loadDump(){
+  /opt/opendj/bin/import-ldif --includeBranch "${BASEDN}" --backendID userRoot --ldifFile "${DUMPLDAP}"
 }
 
-#start server in foreground
 startServer(){
   exec /opt/opendj/bin/start-ds --nodetach
 }
 
-
-if [ "${DUMPLDAP}" != "" ];then
-    install_LDAP
-    stopServer
-    loadFile
-    startServer
-else
-    echo "Configuring LDAP"
-    install_LDAP
-    stopServer
-    startServer
+if [ "${INIT_LDAP}" == "true" ]; then
+    initLdap
 fi
+
+if [ "${LOAD_DUMP}" == "true" ]; then
+    loadDump
+fi
+startServer
